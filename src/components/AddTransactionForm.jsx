90bc434f499a5838d9ae27ab17d24c9736d085f6
Teresa <tenjerim@gmail.com>
2023-01-16 07:48:17 +0300

    return value

--- src/components/AddTransactionForm.jsx
+++ src/components/AddTransactionForm.jsx
@@ -40,3 +40,53 @@ function AddTransactionForm(anewTransaction) {
 
       })
     })
+
+    .then((response)=>response.json())
+    .then((info)=>anewTransaction =(info))
+    console.log(dataInfo);
+    const info =[date, description, category, amount]
+    if(date&&description&&category&&amount){
+      setAdd((dataInfo)=>[...dataInfo,info])
+      setDate("")
+      setDescription("")
+      setCategory("")
+      setAmount("")
+    }
+  }
+
+  function handleChange(event){
+    const val = event.target.name
+     setDataInfo({...dataInfo, [val] : event.target.value})
+  }
+
+  return (
+    <div className="ui segment">
+      <form className="ui form" onSubmit={handleSubmit}>
+        <div className="inline fields">
+          <input type="date" name="date" value={dataInfo.date} onChange={handleChange} />
+          <input type="text" name="description" placeholder="Description" value={dataInfo.description} onChange={handleChange}/>
+          <input type="text" name="category" placeholder="Category" value={dataInfo.category} onChange={handleChange}/>
+          <input type="number" name="amount" placeholder="Amount" step="0.01" value={dataInfo.amount} onChange={handleChange}/>
+        </div>
+        <button className="ui button" type="submit" onClick={handleSubmit}>
+          Add Transaction
+        </button>
+      </form>
+      {
+        add.map((dataInfo)=>
+        <table>
+          <tbody>
+            <tr>
+            <th>{dataInfo.date}</th>
+            <th>{dataInfo.description}</th>
+            <th>{dataInfo.category}</th>
+            <th>{dataInfo.amount}</th>
+            </tr>
+          </tbody>
+        </table>)
+      }
+    </div>
+  );
+}
+
+export default AddTransactionForm;
