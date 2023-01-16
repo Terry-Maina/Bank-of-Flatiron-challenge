e1fb1b87d7e0b15b6277f0b567d2136d8a5505eb
Teresa <tenjerim@gmail.com>
2023-01-16 08:00:24 +0300

    return function

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -15,3 +15,42 @@ function TransactionsList({ transactions, handleDeleteTransaction, handleAddForm
 			console.log(error);
 		}
 	};
+
+
+    return (
+        <table onAddItem= {handleAddForm} className='ui celled striped padded table'>
+          <tbody>
+            <tr>
+            {/* <th>
+                <h3 className="ui center aligned header">#Ref</h3>
+              </th> */}
+              <th>
+                <h3 className='ui center aligned header'>Date</h3>
+              </th>
+              <th>
+                <h3 className='ui center aligned header'>Description</h3>
+              </th>
+              <th>
+                <h3 className='ui center aligned header'>Category</h3>
+              </th>
+              <th>
+                <h3 className='ui center aligned header'>Amount</h3>
+              </th>
+              <th><h3 className='ui center aligned header'>Delete</h3></th>
+            </tr>
+            {/* render a list of <Transaction> components here */}
+            {transactions.map((transaction, idx) => {
+              return (
+                <Transaction
+                key={transaction.id}
+                            transaction={transaction}
+                            deleteTransaction={deleteTransaction}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      );
+    }
+    
+    export default TransactionsList;
