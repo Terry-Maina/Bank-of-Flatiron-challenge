2c830b5ed7037fbc8db25063a3ed8903999078bf
Teresa <tenjerim@gmail.com>
2023-01-19 19:56:44 +0300

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 import AccountContainer from "./AccountContainer";
-import AddTransactionForm from "./AddTransactionForm";
+//import AddTransactionForm from "./AddTransactionForm";
 
 
 function App() {
-  const [transaction, setTransactions] = useState([])
+  //const [transaction, setTransactions] = useState([])
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
